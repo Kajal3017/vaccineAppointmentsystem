26be6e79866206301c880bb1825b8b6de0860b69
Ramandeepkportfolio728 <ramandk.329@gmail.com>
2021-11-29 10:01:18 -0500

    commit by raman

--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from phonenumber_field.modelfields import PhoneNumberField
 
-
-class BookingFormINFO(models.Model):
+class Clinic(models.Model):
     CANADA = 'CA'
     INDIA = 'IN'
     PHONECODE_CHOICES = [
@@ -19,7 +19,7 @@ class BookingFormINFO(models.Model):
     provinces = models.CharField(max_length=50)
 
     phone_code = models.CharField(max_length=5, choices=PHONECODE_CHOICES, default=CANADA)
-    phone_number = models.PositiveIntegerField(validators=[MaxValueValidator(10)])
+    phone_number = models.CharField(max_length=10)
     # website
     website = models.CharField(max_length=500,  blank=True)
     # opening time 
@@ -27,7 +27,6 @@ class BookingFormINFO(models.Model):
     # closing time
     closing_time = models.TimeField()
     # class Vaccine obj
-    vaccine = models.ForeignKey('Vaccine', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.clinic_name
@@ -43,6 +42,7 @@ class Vaccine(models.Model):
         (JAJVaccine, 'Johnson and Johnson vaccine'),
         (AstrazenecaVaccine, 'Astrazeneca vaccine'),
     ]
+    vaccine_for = models.CharField(max_length=500)
     # vacine name
     vaccine_name = models.CharField(max_length=2, choices=VACCINE_CHOICES)
     # is_avaliable
@@ -50,49 +50,84 @@ class Vaccine(models.Model):
     # quantity
     quantity = models.PositiveIntegerField()
 
+    clinic = models.ForeignKey('Clinic', on_delete=models.CASCADE)
+
+
     def __str__(self):
-        return self.vaccine_name + " " + str(self.quantity)
+        # return self.get_vaccine_name_display()
+        return self.vaccine_for + " " + self.vaccine_name + " " + str(self.quantity)
 
 
-# class UserBooking(models.Model):
-#     FIRSTDOSE = 'FD'
-#     SECOUNDOSE = 'SD'
-#     DOSE_CHOICES = [
-#         (FIRSTDOSE, 'First Dose'),
-#         (SECOUNDOSE, 'Last Dose'),
-#     ]
-#     year_in_school = models.CharField(
-#         max_length=2,
-#         choices=YEAR_IN_SCHOOL_CHOICES,
-#         default=FRESHMAN,
-#     )
-#     # firts_name
-#     first_name = models.CharField(max_length=50)
-#     # last_name
-#     last_name = models.CharField(max_length=50)
-#     # user_address
-#     address = models.CharField(max_length=100)
-#     # DOB
-#     DOB = models.DateField()
-#     # phone number
-#     phone_number = models.PositiveIntegerField()
-#     # Health card (image)
-#     health_card = models.ImageField()
-#     # which_dose (1st, 2nd)
-#     which_dose = models.CharField(max_length=2, choices=DOSE_CHOICES)
-#     # vaccine name
-#     vaccine_name = models.CharField()
+class UserAppointBooking(models.Model):
+    CANADA = 'CA'
+    INDIA = 'IN'
+    PHONECODE_CHOICES = [
+        (CANADA, '+1'),
+        (INDIA, '+91'),
+    ]
+    ModernaVaccine = 'MV'
+    PfizerVaccine = 'PV'
+    JAJVaccine = 'JV'
+    AstrazenecaVaccine = 'AV'
+    VACCINE_CHOICES = [
+        (ModernaVaccine, 'Moderna vaccine'),
+        (PfizerVaccine, 'Pfizer vaccine'),
+        (JAJVaccine, 'Johnson and Johnson vaccine'),
+        (AstrazenecaVaccine, 'Astrazeneca vaccine'),
+    ]
 
-#     # date when user will get his/her vaccine
-#     # time of the day
+    FIRST = 'FD'
+    SECOND = 'SD'
+    DOSE_CHOICES = [
+        (FIRST, 'First Dose'),
+        (SECOND, 'Second Dose'),
+    ]
 
-#     #FOR LANDING PAGE
-#     # what you must bring alongwith you while visiting pharmacy or vaccine centre (mandatory things 1)
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+    # firts_name
+    first_name = models.CharField(max_length=50)
+    # last_name
+    last_name = models.CharField(max_length=50)
+    # user_address
+    address_line_one = models.CharField(max_length=100)
+    address_line_two = models.CharField(max_length=100)
 
-#     # Services(2)
-#     # Doctor information(3) 
-#     # About pharmacy ambience or environment (4)
-#     # Donation box(money, blood, clothes and food) (4)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    provinces = models.CharField(max_length=50)
+    # DOB
+    DOB = models.DateField()
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1)
+    # phone number
+    phone_code = models.CharField(max_length=5, choices=PHONECODE_CHOICES, default=CANADA)
+    phone_number = models.CharField(max_length=10)
+    # Health card (image)
+    health_card = models.ImageField(upload_to='images/')
+    # which_dose (1st, 2nd)
+    which_dose = models.CharField(max_length=2, choices=DOSE_CHOICES)
+    # vaccine name
+    vaccine_name = models.CharField(choices=VACCINE_CHOICES, max_length=2)
+
+    # date when user will get his/her vaccine
+    # time of the day
+    choose_date = models.DateTimeField()
+    got_dose = models.BooleanField(default=False)
+    mossed_dose = models.BooleanField(null=True, blank=True)
+
+    #FOR LANDING PAGE
+    # what you must bring alongwith you while visiting pharmacy or vaccine centre (mandatory things 1)
+
+    # Services(2)
+    # Doctor information(3) 
+    # About pharmacy ambience or environment (4)
+    # Donation box(money, blood, clothes and food) (4)
 
 
 
--- main/views.py
+++ main/views.py
@@ -1,39 +1,126 @@
-from django.shortcuts import render, redirect
+from django.http.response import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.conf import settings
 from django.urls import reverse
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 
-from main.forms import DashboardForm, VaccineForm, BookingFormINFOForm
+from main.forms import DashboardForm, VaccineForm, ClinicForm, UserAppointBookingForm
+from main.models import UserAppointBooking, Clinic, Vaccine
+
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+
+
+import stripe
+stripe.api_key = "sk_test_51K0oHcEJY1gb5QocIhDuq1W5WFKbVo59IIF7OrjNW6mWPzctGdIrhURc58koLaKhfP35NR09qlQZYFajpUyZFAnn00j2O6vh1l"
+YOUR_DOMAIN = 'http://localhost:8000/'
+def create_checkout_session(request):
+    if request.method == 'POST':
+        checkout_session = stripe.checkout.Session.create(
+            # payment_method_types=['card'],
+            line_items=[
+                {
+                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                    'price': 'price_1K0upkEJY1gb5Qocr2Luy6Ym',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + 'payment/success/',
+            cancel_url=YOUR_DOMAIN + 'payment/cancel/',
+        )
+    return redirect(checkout_session.url)
+
+
+def donation(request):
+    return render(request, 'main/checkout.html')
+
+    
+def success(request):
+    return render(request, 'main/success.html')
+    
+def cancel(request):
+    return render(request, 'main/cancel.html')
 
 
 def index(request):
-    return HttpResponse("This is going to be the landing page.")
+    return render(request, 'main/homepage.html', {})
 
 
-def add_clinic_info(request):
-    form = BookingFormINFOForm()
+def clinin_list(request):
+    clinic_list = Clinic.objects.all()
+    return render(request, 'main/clinic_list.html', {'clinic_list': clinic_list})
+
+def delete_clinic(request, clinic_id):
+    clinic = get_object_or_404(Clinic, pk=clinic_id)
+    return render(request, 'main/confirm_delete.html', {'clinic': clinic})
+
+
+def delete_it_clinic(request, clinic_id):
+    clinic = get_object_or_404(Clinic, pk=clinic_id)
+    clinic.delete()
+    return redirect('clinic_list')
+
+
+def edit_clinic(request, clinic_id):
+    clinic = get_object_or_404(Clinic, pk=clinic_id)
+    form = ClinicForm(instance=clinic)
+    if request.method == 'POST':
+        form = ClinicForm(request.POST, instance=clinic)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Clinic information updated.')
+            return redirect('clinic_list')
+
+    dictionary = {'form': form}
+    return render(request, 'main/clinic_tem.html', dictionary) 
+
+
+@login_required
+def book_appointment_view(request):
+    form = UserAppointBookingForm()
     if request.method == 'POST' or None:
-        form = BookingFormINFOForm(request.POST)
+        form = UserAppointBookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your appointment is booked.')
+            return redirect('index')
+
+    dictionary = {'form': form}
+    return render(request, 'main/book_appointment.html', dictionary)
+
+
+@login_required
+def add_clinic_info(request):
+    if not request.user.is_superuser:
+        raise PermissionDenied()
+    form = ClinicForm()
+    if request.method == 'POST' or None:
+        form = ClinicForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Clinic info added.')
-            return redirect('index')
+            return redirect('book_appointment')
 
     dictionary = {'form': form}
     return render(request, 'main/clinic_tem.html', dictionary)
 
 
+@login_required
 def add_vaccine(request):
+    if not request.user.is_superuser:
+        raise PermissionDenied()
     form = VaccineForm()
     if request.method == 'POST' or None:
         form = VaccineForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Vaccine quantity added.')
-            return redirect('index')
+            return redirect('add_clinic_info')
 
     dictionary = {'form': form}
     return render(request, 'main/add_vaccine.html', dictionary)
